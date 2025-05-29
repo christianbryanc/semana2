60d171f00cd585fd04a6e1ac45519f304052a16d
Chrisbrayan <bryancris03@gmail.com>
2025-05-29 17:16:06 -0500

    commit final

--- src/tps2/tarea/ejercicio1.java
+++ src/tps2/tarea/ejercicio1.java
@@ -1,33 +1,30 @@
-
 package tps2.tarea;
 
 import java.util.Scanner;
 
-
 public class ejercicio1 {
+
     public static void main(String[] args) {
         //Creando el objeto lectura
         Scanner lectura = new Scanner(System.in);
-        
+
         //Variables
         double radio, area, perimetro;
-        
+
         //Entrada de datos
         System.out.print("Ingrese el radio del circulo: ");
-        radio=lectura.nextDouble();
-        
+        radio = lectura.nextDouble();
+
         //Proceso de datos
-        area=Math.PI * Math.pow(radio,2);
-        perimetro=2*Math.PI*radio;
-        area=Math.round(area);
-        perimetro=Math.round(perimetro);
-  
+        area = Math.PI * Math.pow(radio, 2);
+        perimetro = 2 * Math.PI * radio;
+        area = Math.round(area);
+        perimetro = Math.round(perimetro);
+
         //Salida de datos
-        System.out.println("El area del circulo es: "+area);
-        System.out.println("El perimetro del circulo es: "+perimetro);
-        
-        System.out.println("El perimetro al cuadrado es:"+perimetro*2);
-        
+        System.out.println("El area del circulo es: " + area);
+        System.out.println("El perimetro del circulo es: " + perimetro);
+
     }
-    
+
 }
