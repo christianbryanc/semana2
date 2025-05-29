770410afd51b7f4a6afb122c7539da1e4b38a6ee
Chrisbrayan <bryancris03@gmail.com>
2025-05-29 15:24:41 -0500

    segundo commit

--- src/tps2/tarea/ejercicio1.java
+++ src/tps2/tarea/ejercicio1.java
@@ -26,8 +26,7 @@ public class ejercicio1 {
         System.out.println("El area del circulo es: "+area);
         System.out.println("El perimetro del circulo es: "+perimetro);
         
-        System.out.println("fin del programa");
-        System.out.println("adios");
+       
         
     }
     
